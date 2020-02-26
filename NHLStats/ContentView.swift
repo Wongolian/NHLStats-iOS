9fb070588d145899ee5e23e397a17b2f6f221ea7
Steven Wong <stevenvono@gmail.com>
2020-02-25 18:05:21 -0700

    Initial Commit

---
+++ NHLStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NHLStats
+//
+//  Created by Steven Wong on 2020-02-25.
+//  Copyright © 2020 Steven Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
